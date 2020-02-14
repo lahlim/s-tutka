d8c1b8c04659dded6d01756c2b05457d2db6a9da
lahlim <lassi_mustonen@hotmail.com>
2020-02-14 18:49:25 +0200

    City component data ok

--- src/App.js
+++ src/App.js
@@ -19,17 +19,15 @@ const App = () => {
 
   if (!data.city || !currentData.name) return <h2>Loading...</h2>;
 
-  console.log(currentData);
-
   return (
     <Container>
       <h1 className="text-center p-3">Säätutka</h1>
       <CitySelect />
       <CityWeather data={currentData} />
-      <Row>
+      <Row className="p-2">
         {data.list.map(forecast => {
           return (
-            <Col key={forecast.dt}>
+            <Col key={forecast.dt} className="p-1">
               <CityForecast data={forecast} />
             </Col>
           );
@@ -40,37 +38,77 @@ const App = () => {
 };
 
 const CityWeather = ({ data }) => {
+  console.log(data);
+
+  //  Conver the unit of temp to °C
   const kelvinToCelcius = temp => Math.round(temp - 273.15);
+
+  //  Borrowed from: https://gist.github.com/jlbruno/1535691
+  const getOrdinal = date => {
+    let s = ['th', 'st', 'nd', 'rd'],
+      v = date % 100;
+    return date + (s[(v - 20) % 10] || s[v] || s[0]);
+  };
+
   const iconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+  // Date parsing to proper format
   const date = new Date();
-  const dateToShow = date.toLocaleDateString('en-EN', {
-    month: 'short',
-    day: 'numeric'
-  });
-  const time = date
-    .toLocaleDateString('fi-FI', {
-      hour: '2-digit',
-      minute: '2-digit'
+  let dateToShow = getOrdinal(
+    date.toLocaleDateString('en-EN', {
+      month: 'short',
+      day: 'numeric'
     })
-    .replace('.', ':');
-  console.log(dateToShow);
+  );
+
+  let time = date
+    .toLocaleDateString('fi-FI', {
+      hour: 'numeric',
+      minute: 'numeric'
+    })
+    .split(' ');
+  time = time[2].replace('.', ':');
+
   return (
-    <Card style={{ borderColor: '#E6E6E6' }} className="text-center p-2 mb-2">
+    <Card style={{ borderColor: '#E6E6E6' }} className=" p-2 mb-2">
       <Row>
         <Col>
           <h2>{data.name}</h2>
           <p>{data.weather[0].description}</p>
         </Col>
-        <Col>
+        <Col
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end'
+          }}
+        >
           <img src={iconUrl} alt="Weather icon"></img>
           <p style={{ height: '13px' }}>{kelvinToCelcius(data.main.temp)}°C</p>
         </Col>
       </Row>
       <Row>
-        <Col>
-          {dateToShow} {time}
+        <Col className="align-items end">
+          {dateToShow}
+          <br />
+          {time}
         </Col>
-        <Col>asd</Col>
+        <Col
+          style={{
+            listStyleType: 'none',
+            fontSize: '13px',
+            color: ' #70757A',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end'
+          }}
+        >
+          <li>Wind: {data.wind.speed} m/s</li>
+
+          <li>Humidity: {data.main.humidity} %</li>
+          <li>Precipitation (3 h): {data.main.humidity} mm TODO FIX</li>
+        </Col>
       </Row>
     </Card>
   );
--- src/services/weather.js
+++ src/services/weather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getWeatherForecast = () => {
   const request = axios.get(
-    'http://api.openweathermap.org/data/2.5/forecast?id=634964&cnt=5&APIKEY=9baeaf613a5f530b584aef82691010d3'
+    'http://api.openweathermap.org/data/2.5/forecast?id=634964&cnt=6&APIKEY=9baeaf613a5f530b584aef82691010d3'
   );
   return request.then(response => response.data);
 };
